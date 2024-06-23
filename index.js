298cebbfd1a1a54bf549869b2cdb08afd4876a0a
5anne <tahaminaanne@gmail.com>
2024-06-23 14:32:31 +0600

    sixth commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -32,11 +33,45 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
+        await client.connect();
 
         const allMealsCollection = client.db('dineDB').collection('allMeals');
         const userInfoDB = client.db('dineDB').collection('userInfo');
         const mealJsonDB = client.db('dineDB').collection('mealJson');
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            });
+            res.send({ token });
+        })
+
+        const verifyToken = (req, res, next) => {
+            console.log('Inside Verify Token', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' });
+            }
+            const token = req.headers.authorization.split(' ')[1]
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' });
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userInfoDB.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            next();
+        }
 
         app.get("/allMeals", async (req, res) => {
             const cursor = allMealsCollection.find();
@@ -56,9 +91,23 @@ async function run() {
             res.send(result);
         })
 
-        app.get("/userInfo", async (req, res) => {
+        app.get("/userInfo", verifyToken, verifyAdmin, async (req, res) => {
             const result = await userInfoDB.find().toArray();
             res.send(result);
+        })
+
+        app.get('/userInfo/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            const query = { email: email };
+            const user = await userInfoDB.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+            res.send({ admin });
         })
 
         app.post('/userInfo', async (req, res) => {
@@ -104,10 +153,9 @@ async function run() {
         //     res.send(result);
         // })
 
-        app.patch('/userInfo/admin/:id', async (req, res) => {
+        app.patch('/userInfo/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
-            // const options = { upsert: true };
             const updateDoc = {
                 $set: {
                     role: 'admin'
@@ -118,7 +166,7 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
+        await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
