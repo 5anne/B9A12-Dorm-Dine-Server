51fb65353481462492e3d1d24d009433213019b3
5anne <tahaminaanne@gmail.com>
2024-09-09 14:01:24 +0600

    updated functionality

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const stripe = require('stripe')(process.env.STRPE_SECRET_KEY)
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(
-    cors({
-        origin: [
-            "http://localhost:5173",
-            "https://dorm-dine-6deee.web.app",
-            "https://dorm-dine-6deee.firebaseapp.com",
-        ]
-    })
-);
+app.use(cors());
 app.use(express.json());
 
 
@@ -91,90 +83,12 @@ async function run() {
             res.send(result);
         })
 
-        app.get("/upcomingMeals", async (req, res) => {
-            const result = await upcomingMealsCollection.find().toArray();
-            res.send(result);
-        })
-        
-        app.get("/usersAct", async (req, res) => {
-            const result = await usersActivity.find().toArray();
-            res.send(result);
-        })
-
-        app.get("/premiumJson", async (req, res) => {
-            const result = await premiumJsonDB.find().toArray();
-            res.send(result);
-        })
-
-        app.get('/premiumJson/:badge', async (req, res) => {
-            const badge = req.params.badge;
-            const query = { badge: badge };
-            const result = await premiumJsonDB.findOne(query);
-            res.send(result);
-        })
-
-        app.get("/requestedMeals", verifyToken, async (req, res) => {
-            const result = await requestedMealsCollection.find().toArray();
-            res.send(result);
-        })
-
-        app.get('/requestedMeals/:name', verifyToken, verifyAdmin, async (req, res) => {
-            const query = { name: req.params.name }
+        app.get('/allMealsEmail/:email', async (req, res) => {
+            const query = { admin_email: { $regex: new RegExp(req.params.email, '') } }
             console.log(query);
-            const result = await requestedMealsCollection.find(query).toArray();
-            res.send(result);
-        })
-
-        app.get("/userInfo", verifyToken, async (req, res) => {
-            const result = await userInfoDB.find().toArray();
-            res.send(result);
-        })
-
-        app.get('/userInfo/:name', verifyToken, verifyAdmin, async (req, res) => {
-            const query = { name: req.params.name }
-            const result = await userInfoDB.find(query).toArray();
-            res.send(result);
-        })
-
-        app.get('/userInfo/:email', verifyToken, verifyAdmin, async (req, res) => {
-            const query = { email: req.params.email }
-            const result = await userInfoDB.find(query).toArray();
-            res.send(result);
-        })
-
-        app.get('/userInfo/admin/:email', verifyToken, async (req, res) => {
-            const email = req.params.email;
-            if (email !== req.decoded.email) {
-                return res.status(403).send({ message: 'forbidden access' })
-            }
-            const query = { email: email };
-            const user = await userInfoDB.findOne(query);
-            let admin = false;
-            if (user) {
-                admin = user?.role === 'admin';
-            }
-            res.send({ admin });
-        })
-
-        app.get('/payments/:email', verifyToken, async (req, res) => {
-            const query = { email: req.params.email }
-            if (req.params.email !== req.decoded.email) {
-                return res.status(403).send({ message: 'forbidden access' });
-            }
-            const result = await paymentJsonDB.find(query).toArray();
-            res.send(result);
-        })
-
-        app.post('/userInfo', async (req, res) => {
-            const user = req.body;
-            console.log(user);
-            const query = { email: user.email }
-            const existingUser = await userInfoDB.findOne(query);
-            if (existingUser) {
-                return res.send({ message: 'User already exists!', insertedId: null })
-            }
-            const result = await userInfoDB.insertOne(user);
-            res.send(result);
+            const mealCount = await allMealsCollection.find(query).toArray();
+            console.log(mealCount);
+            res.send(mealCount);
         })
 
         app.post('/allMeals', verifyToken, verifyAdmin, async (req, res) => {
@@ -183,81 +97,7 @@ async function run() {
             res.send(result);
         })
 
-        app.post('/requestedMeals', verifyToken, async (req, res) => {
-            const meal = req.body;
-            const result = await requestedMealsCollection.insertOne(meal);
-            res.send(result);
-        })
-
-        app.post('/upcomingMeals', verifyToken, verifyAdmin, async (req, res) => {
-            const meal = req.body;
-            const result = await upcomingMealsCollection.insertOne(meal);
-            res.send(result);
-        })
-
-        app.post('/usersAct', async (req, res) => {
-            const meal = req.body;
-            const result = await usersActivity.insertOne(meal);
-            res.send(result);
-        })
-
-        app.delete('/upcomingMeals/:id', verifyToken, verifyAdmin, async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await upcomingMealsCollection.deleteOne(query);
-            res.send(result);
-        })
-
-        app.delete('/allMeals/:id', verifyToken, verifyAdmin, async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await allMealsCollection.deleteOne(query);
-            res.send(result);
-        })
-
-        app.delete('/requestedMeals/:id', verifyToken, async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await requestedMealsCollection.deleteOne(query);
-            res.send(result);
-        })
-
-        app.delete('/usersAct/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await usersActivity.deleteOne(query);
-            res.send(result);
-        })
-
-        app.patch('/userInfo/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) };
-            const updateDoc = {
-                $set: {
-                    role: 'admin'
-                }
-            }
-            const result = await userInfoDB.updateOne(filter, updateDoc);
-            res.send(result);
-        })
-
-        app.patch('/userInfo/:email', async (req, res) => {
-            const meal = req.body;
-            const email = req.params.email;
-            const filter = { email: email }
-            const updateDoc = {
-                $set: {
-                    name: meal.name,
-                    email: meal.email,
-                    photo: meal.photo,
-                    userBadge: meal.userBadge
-                }
-            }
-            const result = await userInfoDB.updateOne(filter, updateDoc);
-            res.send(result);
-        })
-
-        app.patch('/allMeals/:id', async (req, res) => {
+        app.patch('/allMeals/:id', verifyToken, verifyAdmin, async (req, res) => {
             const meal = req.body;
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) }
@@ -280,6 +120,111 @@ async function run() {
                 }
             }
             const result = await allMealsCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.delete('/allMeals/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await allMealsCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        app.get("/upcomingMeals", async (req, res) => {
+            const result = await upcomingMealsCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.post('/upcomingMeals', verifyToken, verifyAdmin, async (req, res) => {
+            const meal = req.body;
+            const result = await upcomingMealsCollection.insertOne(meal);
+            res.send(result);
+        })
+
+        app.patch('/upcomingMeals/:id', verifyToken, async (req, res) => {
+            const meal = req.body;
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    title: meal.title,
+                    category: meal.category,
+                    image: meal.image,
+                    ingredients: meal.ingredients,
+                    description: meal.description,
+                    price: meal.price,
+                    rating: meal.rating,
+                    post_time: meal.post_time,
+                    likes: meal.likes,
+                    reviews: meal.reviews,
+                    reviewText: meal.reviewText,
+                    status: meal.status,
+                    admin_name: meal.admin_name,
+                    admin_email: meal.admin_email
+                }
+            }
+            const result = await upcomingMealsCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.delete('/upcomingMeals/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await upcomingMealsCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        app.get("/usersAct", async (req, res) => {
+            const result = await usersActivity.find().toArray();
+            res.send(result);
+        })
+
+        app.post('/usersAct', async (req, res) => {
+            const meal = req.body;
+            const result = await usersActivity.insertOne(meal);
+            res.send(result);
+        })
+
+        app.delete('/usersAct/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await usersActivity.deleteOne(query);
+            res.send(result);
+        })
+
+        app.get("/premiumJson", async (req, res) => {
+            const result = await premiumJsonDB.find().toArray();
+            res.send(result);
+        })
+
+        app.get('/premiumJson/:badge', async (req, res) => {
+            const badge = req.params.badge;
+            const query = { badge: badge };
+            const result = await premiumJsonDB.findOne(query);
+            res.send(result);
+        })
+
+        app.get("/requestedMeals", verifyToken, async (req, res) => {
+            const result = await requestedMealsCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.get('/requestedMeals/:name', verifyToken, verifyAdmin, async (req, res) => {
+            const query = { user_name: { $regex: new RegExp(req.params.name, 'i') } }
+            console.log(query);
+            const result = await requestedMealsCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.get('/requestedMealsEmail/:email', async (req, res) => {
+            const query = { user_email: { $regex: new RegExp(req.params.email, 'i') } }
+            const result = await requestedMealsCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.post('/requestedMeals', verifyToken, async (req, res) => {
+            const meal = req.body;
+            const result = await requestedMealsCollection.insertOne(meal);
             res.send(result);
         })
 
@@ -311,29 +256,93 @@ async function run() {
             res.send(result);
         })
 
-        app.patch('/upcomingMeals/:id', verifyToken, async (req, res) => {
-            const meal = req.body;
+        app.delete('/requestedMeals/:id', verifyToken, async (req, res) => {
             const id = req.params.id;
-            const filter = { _id: new ObjectId(id) }
+            const query = { _id: new ObjectId(id) }
+            const result = await requestedMealsCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        app.get("/userInfo", async (req, res) => {
+            const result = await userInfoDB.find().toArray();
+            res.send(result);
+        })
+
+        app.get('/userInfo/:name', async (req, res) => {
+            // const query = { name: req.params.name }
+            const query = { name: { $regex: new RegExp(req.params.name, 'i') } }
+            const result = await userInfoDB.find(query).toArray();
+            res.send(result);
+        })
+
+        app.get('/userInfoEmail/:email', async (req, res) => {
+            // const query = { email: req.params.email }
+            const query = { email: { $regex: new RegExp(req.params.email, 'i') } }
+            // console.log(query);
+            const result = await userInfoDB.find(query).toArray();
+            res.send(result);
+        })
+
+        app.get('/userInfo/admin/:email', verifyToken, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            const query = { email: email };
+            const user = await userInfoDB.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+            res.send({ admin });
+        })
+
+        app.post('/userInfo', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const query = { email: user.email }
+            const existingUser = await userInfoDB.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'User already exists!', insertedId: null })
+            }
+            const result = await userInfoDB.insertOne(user);
+            res.send(result);
+        })
+
+        app.patch('/userInfo/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
             const updateDoc = {
                 $set: {
-                    title: meal.title,
-                    category: meal.category,
-                    image: meal.image,
-                    ingredients: meal.ingredients,
-                    description: meal.description,
-                    price: meal.price,
-                    rating: meal.rating,
-                    post_time: meal.post_time,
-                    likes: meal.likes,
-                    reviews: meal.reviews,
-                    reviewText: meal.reviewText,
-                    status: meal.status,
-                    admin_name: meal.admin_name,
-                    admin_email: meal.admin_email
+                    role: 'admin'
                 }
             }
-            const result = await upcomingMealsCollection.updateOne(filter, updateDoc);
+            const result = await userInfoDB.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.patch('/userInfo/:email', async (req, res) => {
+            const meal = req.body;
+            const email = req.params.email;
+            const filter = { email: email }
+            const updateDoc = {
+                $set: {
+                    name: meal.name,
+                    email: meal.email,
+                    photo: meal.photo,
+                    userBadge: meal.userBadge
+                }
+            }
+            const result = await userInfoDB.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.get('/payments/:email', verifyToken, async (req, res) => {
+            const query = { email: req.params.email }
+            if (req.params.email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            const result = await paymentJsonDB.find(query).toArray();
             res.send(result);
         })
 
